b17f3163f9a44bf94fdc0023d7a55586ec5a4b6b
Kurt Steigleder <ksteigleder@brex.com>
2026-01-08 16:18:22 -0800

    syntax highlighting for groq inside a defineQuery() call works

--- editors/nvim-injections.scm
+++ editors/nvim-injections.scm
@@ -1,15 +1,37 @@
 ; extends
 
-; GROQ injection for /* groq */ `...` pattern in TypeScript/JavaScript
+; GROQ injection for TypeScript/JavaScript
 ;
 ; Installation: Copy to ~/.config/nvim/after/queries/typescript/injections.scm
 ;                   and ~/.config/nvim/after/queries/javascript/injections.scm
 
-; Matches: const x = /* groq */ `...`
-; AST: (variable_declarator (comment) value: (template_string (string_fragment)))
+; =============================================================================
+; Pattern 1: const x = /* groq */ `...`
+; =============================================================================
 ((variable_declarator
   (comment) @_groq_comment
   value: (template_string
     (string_fragment) @injection.content))
   (#lua-match? @_groq_comment "groq")
+  (#set! injection.language "groq"))
+
+; =============================================================================
+; Pattern 2: defineQuery(`...`)
+; =============================================================================
+((call_expression
+  function: (identifier) @_fn
+  arguments: (arguments
+    (template_string
+      (string_fragment) @injection.content)))
+  (#eq? @_fn "defineQuery")
+  (#set! injection.language "groq"))
+
+; =============================================================================
+; Pattern 3: groq`...` (tagged template literal)
+; =============================================================================
+((call_expression
+  function: (identifier) @_fn
+  arguments: (template_string
+    (string_fragment) @injection.content))
+  (#eq? @_fn "groq")
   (#set! injection.language "groq"))
